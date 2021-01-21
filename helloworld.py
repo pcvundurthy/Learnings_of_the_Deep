1fcda0b03b1146c6ec9b387ec9dabcca09c23ae7
Pavan Chandra Vundurthy <pcvundurthy@gmail.com>
2021-01-21 12:58:17 +0530

    updated pthon code for grading houses using ML

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+#graded House Model
+
+def house_model(y_new):
+	xs=np.array([1,2,3,4,5,6],dtype=int)
+	ys=np.array([1.0,1.5,2.0,2.5,3.0,3.5],dtype=float)
+	model= tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+	model.compile(optimizer='sgd', loss='mean_squared_error')
+	model.fit(xs,ys,epochs=500)
+	return model.predict(y_new)[0]
+prediction=house_model([7.0])
+print(prediction)
